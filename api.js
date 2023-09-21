fd49e1ef379f3bf4c5b6ec7fe3cfea0840b2efcc
Aitor Igea <igeaitor@gmail.com>
2023-09-21 18:35:02 +0200

    directores bien

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-document.getElementById("enviar").addEventListener("click", hacerSolicitudAPI);
+/*document.getElementById("enviar").addEventListener("click", hacerSolicitudAPI);
 
 function hacerSolicitudAPI(){
 	var nombrePeli = document.getElementById("nombrep").value;
@@ -33,7 +33,37 @@ function hacerSolicitudAPI(){
 	.catch(function(error){
 		alert("error");
 		});
-	});
+	};
 
-	
+	*/
+
+document.getElementById("enviar").addEventListener("click", hacerSolicitudAPI);
+
+function hacerSolicitudAPI(event) {
+  event.preventDefault(); // Evita que el formulario se envíe y recargue la página.
+
+  var nombrePeli = document.getElementById("nombrep").value;
+  var apiURL = "https://www.omdbapi.com/?t=" + encodeURIComponent(nombrePeli); // Agregar el nombre de la película a la URL.
+  var apiKEY = "c74b4aea";
+
+  fetch(apiURL + "&apikey=" + apiKEY)
+    .then(function (response) {
+      if (!response.ok) {
+        alert("Error al buscar la película");
+        return;
+      }
+
+      response.json().then(function (data) {
+        if (data.Error) {
+          alert("No se encontró información para la película");
+        } else {
+          var director = data.Director;
+		  alert(director);
+          document.getElementById("director").textContent = "Director: " + director;
+        }
+      });
+    })
+    .catch(function (error) {
+      alert("Error en la solicitud: " + error.message);
+    });
 }
